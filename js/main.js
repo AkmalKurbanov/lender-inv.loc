de34083f5a901fa908ef7fb4840bef59a5b9b981
AkmalKurbanov <250296a@gmail.com>
2022-10-18 16:32:30 +0600

    edited deal list, added accordion in profile

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$('document').ready(function () {
+  $('.file-btn').click(function () {
+    $(this).closest('label').closest('div').find('input');
+  });
+
+  $('.file-input').on('change', function () {
+    let fileName = $(this).val().split('\\').pop();
+    $(this).closest('div').find('label').find('span').text(fileName);
+    $(this).closest('div').find('.remove').show();
+  });
+
+  $('.remove').click(function () {
+    $(this).closest('div').find('input').val('');
+    $(this).closest('div').find('span').text('');
+    $(this).hide();
+  });
+
+  //смена пользователя
+  $('.tools .ava-control').on('click', function () {
+    $(this).closest('.tools').addClass('open');
+  });
+
+  $(document).mouseup(function (e) {
+    // событие клика по веб-документу
+    let div = $('.tools'); // тут указываем ID элемента
+    if (
+      !div.is(e.target) && // если клик был не по нашему блоку
+      div.has(e.target).length === 0
+    ) {
+      // и не по его дочерним элементам
+      div.removeClass('open'); // скрываем его
+    }
+  });
+});
+
+
+
+
+// accordeon
+$('.accordeon .title-profilw').on('click', function () {
+  $(this).parent().parent().find('.accordeon__hidden-block').slideToggle();
+  $(this).parent().parent().toggleClass('open');
+});
+// accordeon end
